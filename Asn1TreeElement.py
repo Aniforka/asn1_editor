6e541b2e2a6c985afd80d8d20b89908d97a00eb0
Gavrilov Dmitrii <gavrilovdv1@student.bmstu.ru>
2024-07-08 07:59:09 +0300

    ADD: getters

--- Asn1TreeElement.py
+++ Asn1TreeElement.py
@@ -9,7 +9,7 @@ class Asn1TreeElement:
             uid=-1)-> None:
         
         self.parent = parent
-        self.childs = None
+        self.childs = list()
         self.decode_value = decode_value
         self.encode_value = encode_value
         self.tag_type = tag_type
@@ -18,16 +18,28 @@ class Asn1TreeElement:
         self.uid = uid
 
     def add_child(self, element) -> None:
-        if self.childs is None:
-            self.childs = list()
-
         self.child.append(element)
-
-    def get_parrent(self):
-        return self.parent
 
     def get_length(self) -> int:
         return self.length
 
     def get_offset(self) -> int:
         return self.offset
+
+    def get_decode_value(self) -> str:
+        return self.decode_value
+    
+    def get_encode_value(self):
+        return self.encode_value
+    
+    def get_tag_type(self) -> str:
+        return self.tag_type
+    
+    def get_uid(self) -> int:
+        return self.uid
+
+    def get_parrent(self):
+        return self.parent
+    
+    def get_childs(self):
+        return self.childs
