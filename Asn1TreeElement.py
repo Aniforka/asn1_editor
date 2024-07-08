e33487b686777d04c850cda14dda85b573b3e09c
Gavrilov Dmitrii <gavrilovdv1@student.bmstu.ru>
2024-07-08 04:20:43 +0300

    FIXED: init

--- Asn1TreeElement.py
+++ Asn1TreeElement.py
@@ -1,9 +1,15 @@
 class Asn1TreeElement:
-    def __init__(self) -> None:
-        childs = None
-        decode_value = None
-        encode_value = None
-        tag_type = None
-        length = None
-        offset = None
-        uid = None
+    def __init__(
+            self,
+            decode_value=None,
+            encode_value=None, tag_type=None,
+            length=None,
+            offset=None,
+            uid=None)-> None:
+        self.childs = None
+        self.decode_value = decode_value
+        self.encode_value = encode_value
+        self.tag_type = tag_type
+        self. length = length
+        self.offset = offset
+        self.uid = uid
