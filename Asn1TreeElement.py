6a063cf8e19e25bfeff0772a4269ee2d1bd38673
Gavrilov Dmitrii <gavrilovdv1@student.bmstu.ru>
2024-07-08 07:54:04 +0300

    FIXED: attributes and returns

--- Asn1TreeElement.py
+++ Asn1TreeElement.py
@@ -4,16 +4,16 @@ class Asn1TreeElement:
             parent=None,
             decode_value=None,
             encode_value=None, tag_type=None,
-            length=None,
-            offset=None,
-            uid=None)-> None:
+            length=-1,
+            offset=-1,
+            uid=-1)-> None:
         
         self.parent = parent
         self.childs = None
         self.decode_value = decode_value
         self.encode_value = encode_value
         self.tag_type = tag_type
-        self. length = length
+        self.length = length
         self.offset = offset
         self.uid = uid
 
@@ -22,3 +22,12 @@ class Asn1TreeElement:
             self.childs = list()
 
         self.child.append(element)
+
+    def get_parrent(self):
+        return self.parent
+
+    def get_length(self) -> int:
+        return self.length
+
+    def get_offset(self) -> int:
+        return self.offset
