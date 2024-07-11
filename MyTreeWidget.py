faeb7d52796cd6035b9a5f1437c0f1b40c4bdc02
Gavrilov Dmitrii <dmitrii.gavrilov.dev@gmail.com>
2024-07-11 17:23:08 +0300

    ADD: context menu

--- MyTreeWidget.py
+++ MyTreeWidget.py
@@ -1,33 +1,44 @@
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QMenu, QAction, QApplication
 
+from Asn1Tree import Asn1Tree
+
 class MyTreeWidget(QTreeWidget):
     def __init__(self):
         super().__init__()
 
         # Создаем действия для контекстного меню
+        self.action_create = QAction("Создать", self)
         self.action_edit = QAction("Редактировать", self)
         self.action_delete = QAction("Удалить", self)
 
-        # Подключаем сигналы к слотам (опционально)
+        self.action_create.triggered.connect(self.create_item)
         self.action_edit.triggered.connect(self.edit_item)
         self.action_delete.triggered.connect(self.delete_item)
 
     def contextMenuEvent(self, event):
         # Создаем контекстное меню
         menu = QMenu(self)
+        menu.addAction(self.action_create)
         menu.addAction(self.action_edit)
         menu.addAction(self.action_delete)
+
+        item = self.currentItem()
+        is_item_valid = len(item.asn1_tree_element.get_childs())  # Замените на вашу логику проверки
+
+        # Управление состоянием действия "Создать"
+        self.action_create.setEnabled(is_item_valid)
 
         # Отображаем меню на позиции курсора
         menu.exec_(event.globalPos())
 
+    def create_item(self):
+        item = self.currentItem()
+
     def edit_item(self):
-        # Обработчик действия "Редактировать"
         item = self.currentItem()
         print(f"Редактируем элемент: {item.text(0)}")
 
     def delete_item(self):
-        # Обработчик действия "Удалить"
         item = self.currentItem()
         print(f"Удаляем элемент: {item.text(0)}")
 
